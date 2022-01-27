1d45d6be15ef135bd7ad6c556a8ba96e6235de37
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-26 19:46:33 -0600

    Finished the walkEast fuction

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,51 +1,57 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
+    let element = newImage("assets/red-character/static.gif");
     element.style.zIndex = 1;
-    
+
     let direction = null;
 
     function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
+        if (direction === "west") {
+            x -= 1;
         }
-        if (direction === 'north') {
-            y += 1
+        if (direction === "north") {
+            y += 1;
         }
-        if (direction === 'east') {
-            x += 1
+        if (direction === "east") {
+            x += 1;
         }
-        if (direction === 'south') {
-            y -= 1
+        if (direction === "south") {
+            y -= 1;
         }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+        element.style.left = x + "px";
+        element.style.bottom = y + "px";
     }
 
-    setInterval(moveCharacter, 1)
+    setInterval(moveCharacter, 1);
 
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    function walkEast(time, callback) {
+        direction = "east";
+        element.src = `./assets/green-character/east.gif`;
+        setTimeout(() => {
+            stop();
+            if (callback) {
+                callback();
+            }
+        }, time);
     }
 
     function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+        direction = "north";
+        element.src = `./assets/red-character/north.gif`;
     }
 
     function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+        direction = "west";
+        element.src = `./assets/red-character/west.gif`;
     }
 
     function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+        direction = "south";
+        element.src = `./assets/red-character/south.gif`;
     }
 
     function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
+        direction = null;
+        element.src = `./assets/red-character/static.gif`;
     }
 
     return {
@@ -54,6 +60,6 @@ function newNonPlayableCharacter(x, y) {
         walkNorth: walkNorth,
         walkEast: walkEast,
         walkSouth: walkSouth,
-        stop: stop
-    }
+        stop: stop,
+    };
 }
